88c0e55f2ce3e4a4ee13ce535dac5423112dcefd
Tw <wei.tan@intel.com>
2020-05-27 16:31:28 +0800

    tw; add qemu_tw.sh
    Signed-off-by: Tw <wei.tan@intel.com>

---
+++ acrn-unit-test/guest/qemu_tw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# set -x
+
+[ "$1" = '--help' ] && exit 1;
+
+killall qemu-system-x86_64
+
+./grub_iso acrn-unit_test.iso ~/work/fusa/acrn-sliced-mainline/hypervisor/build/acrn.32.out $@
+
+qemu-system-x86_64 -machine q35,kernel_irqchip=split,accel=kvm -cpu max,level=22,invtsc -m 4G -smp cpus=8,cores=4,threads=2 -enable-kvm -device isa-debug-exit -device intel-iommu,intremap=on,aw-bits=48,caching-mode=on,device-iotlb=on -debugcon file:/dev/stdout -serial mon:stdio -display none  -boot d -cdrom ./acrn-unit_test.iso || true
+
+

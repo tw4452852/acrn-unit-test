76aa419db76a78cb4059930daf2fc546728d41bf
Tw <wei.tan@intel.com>
2020-05-28 15:54:08 +0800

    qemu_tw: specify physical address width to 39
    Signed-off-by: Tw <wei.tan@intel.com>

--- acrn-unit-test/guest/qemu_tw.sh
+++ acrn-unit-test/guest/qemu_tw.sh
@@ -8,6 +8,4 @@ killall qemu-system-x86_64
 
 ./grub_iso acrn-unit_test.iso ~/work/fusa/acrn-sliced-mainline/hypervisor/build/acrn.32.out $@
 
-qemu-system-x86_64 -machine q35,kernel_irqchip=split,accel=kvm -cpu max,level=22,invtsc -m 4G -smp cpus=8,cores=4,threads=2 -enable-kvm -device isa-debug-exit -device intel-iommu,intremap=on,aw-bits=48,caching-mode=on,device-iotlb=on -debugcon file:/dev/stdout -serial mon:stdio -display none  -boot d -cdrom ./acrn-unit_test.iso || true
-
-
+qemu-system-x86_64 -machine q35,kernel_irqchip=split,accel=kvm -cpu max,level=22,invtsc,phys-bits=39 -m 4G -smp cpus=8,cores=4,threads=2 -enable-kvm -device isa-debug-exit -device intel-iommu,intremap=on,aw-bits=48,caching-mode=on,device-iotlb=on -debugcon file:/dev/stdout -serial mon:stdio -display none  -boot d -cdrom ./acrn-unit_test.iso || true
